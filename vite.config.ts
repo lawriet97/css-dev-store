d56c5785013698cb501095b00bca5027065620c3
cleilsonsilva <129252350+lawriet97@users.noreply.github.com>
2026-05-01 10:39:32 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/css-dev-store/',
+  base: '/', // Altere de '/css-dev-store/' para '/'
 })
